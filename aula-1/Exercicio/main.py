f5c21b9aa7c89e78988d2dced36e8dab10ea84f9
LAURO HENRIQUE ABAD VIANNA <86790307087@senacrs.edu.br>
2026-03-25 10:01:20 -0300

    Aula 04 - Exercicio

---
+++ aula-1/Exercicio/main.py
@@ -0,0 +1,17 @@
+from Lista import Lista
+
+lista = Lista()
+
+lista.imprimir()
+
+lista.add( "João" )
+lista.add( "Antônio" )
+lista.add( "Gabriel")
+lista.add( "Antoni" )
+lista.add( "Lorenzo" )
+
+lista.remover( "João" )
+lista.remover( "ABELÃO" )
+lista.remover( "Gabriel")
+lista.remover( "Antoni" )
+lista.remover( "Lorenzo" )
